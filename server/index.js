efc321a531cb6d907b6b67360fc18d82c32fa8c7
Ankur Saxena <ankursaxenaankur0@gmail.com>
2022-09-20 12:50:35 +0530

    port fixing

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const express = require('express')
 const socketIO = require('socket.io')
 
 const app = express()
-const port = process.env.port || 5000
+const port = process.env.port
 
 const pathname = path.join(__dirname, '/../public')
 console.log(pathname);
